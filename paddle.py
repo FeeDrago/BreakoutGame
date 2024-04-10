19332e19762254e0680bb047f15e49f6bc093d56
Nikos <ngeorganakis@gmail.com>
2024-04-10 17:44:48 +0300

    Remove not used files

--- paddle.py
+++
@@ -1,8 +0,0 @@
-import pygame
-from constants import *
-
-
-class Paddle(pygame.Rect):
-    def __init__(self, left, top, width, height, color):
-        super().__init__(left, top, width, height)
-        self.color = color
